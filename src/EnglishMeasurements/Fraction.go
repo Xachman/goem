c1f7948d91c04cad1ec7e62ec9e5abd79653df05
xach <zironside@hotmail.com>
2017-06-04 07:25:36 -0400

    add multiply subtract divide to fraction

--- src/EnglishMeasurements/Fraction.go
+++ src/EnglishMeasurements/Fraction.go
@@ -14,6 +14,27 @@ func (f *Fraction) Add(fraction *Fraction) *Fraction {
 	return f.simplifyF(&newFrac);
 }
 
+func (f *Fraction) Divide(fraction *Fraction) *Fraction {
+	numerator := f.Numerator * fraction.Denominator;
+	denominator := fraction.Numerator * f.Denominator;
+	return f.simplify(numerator, denominator);
+}
+
+func (f *Fraction) Multiply(fraction *Fraction) *Fraction {
+	numerator := f.Numerator * fraction.Numerator;
+	denominator := fraction.Denominator * f.Denominator;
+	return f.simplify(numerator, denominator);
+}
+
+
+func (f *Fraction) Subtract(fraction *Fraction) *Fraction {
+	commonDenominator := f.Denominator * fraction.Denominator;
+	numeratorOne := fraction.Numerator * f.Denominator;
+	numeratorTwo := f.Numerator * fraction.Denominator;
+	subNumerator := numeratorOne - numeratorTwo;
+	return f.simplify(subNumerator, commonDenominator);
+}
+
 func (f *Fraction) simplify(num, den int) *Fraction {
 	greatestCommonDivisor := gcd(num, den);
 	newFrac := Fraction{num / greatestCommonDivisor, den / greatestCommonDivisor};
--- test/Fraction_test.go
+++ test/Fraction_test.go
@@ -2,7 +2,10 @@ package Test
 
 
 import "testing"
-import "EnglishMeasurements"
+import (
+	"EnglishMeasurements"
+	"fmt"
+)
 
 
 func TestFractionAdd(t *testing.T) {
@@ -26,3 +29,67 @@ func TestFractionAdd(t *testing.T) {
 		t.Error("Expected Numerator 1 but got ", fraction3.Numerator);
 	}
 }
+
+
+func TestFractionDivide(t *testing.T) {
+	fraction1 := EnglishMeasurements.Fraction{
+		Numerator: 2,
+		Denominator: 3,
+	}
+
+	fraction2 :=  EnglishMeasurements.Fraction{
+		Numerator: 1,
+		Denominator: 4,
+	}
+
+	fraction3 := fraction1.Divide(&fraction2);
+
+	testNumDen(8, 3, fraction3, t)
+
+}
+
+func TestFractionMultiply(t *testing.T) {
+	fraction1 := EnglishMeasurements.Fraction{
+		Numerator: 2,
+		Denominator: 3,
+	}
+
+	fraction2 :=  EnglishMeasurements.Fraction{
+		Numerator: 5,
+		Denominator: 7,
+	}
+
+	fraction3 := fraction1.Multiply(&fraction2);
+
+	testNumDen(10, 21, fraction3, t)
+
+}
+
+func TestFractionSubtract(t *testing.T) {
+	fraction1 := EnglishMeasurements.Fraction{
+		Numerator: 3,
+		Denominator: 4,
+	}
+
+	fraction2 :=  EnglishMeasurements.Fraction{
+		Numerator: 8,
+		Denominator: 10,
+	}
+
+	fraction3 := fraction1.Subtract(&fraction2);
+
+	testNumDen(1, 20, fraction3, t)
+
+}
+
+func testNumDen(num, den int, f *EnglishMeasurements.Fraction, t *testing.T) {
+
+	if den != f.Denominator {
+		t.Error(fmt.Sprintf("Expected Denominator %d but got ", den), f.Denominator);
+	}
+
+	if f.Numerator != num {
+		t.Error(fmt.Sprintf("Expected Numerator %d but got ", num), f.Numerator);
+	}
+}
+
