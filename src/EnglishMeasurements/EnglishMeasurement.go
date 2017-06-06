f9895cc93192ccaa51e242fdec8443065ce6070d
xach <zironside@hotmail.com>
2017-06-05 21:06:19 -0400

    pass tests for subtact and add EnglishMeasurement

--- src/EnglishMeasurements/EnglishMeasurement.go
+++ src/EnglishMeasurements/EnglishMeasurement.go
@@ -1,30 +1,44 @@
 package EnglishMeasurements
 
+
 type EnglishMeasurement struct {
-	Fraction Fraction;
+	fraction Fraction;
 }
 
 const cFOOT  = 12
-func (e *EnglishMeasurement) Add(em *EnglishMeasurement) (*EnglishMeasurement) {
-	newFrac := em.Fraction.Add(&e.Fraction);
+func (e *EnglishMeasurement) Add(em EnglishMeasurement) EnglishMeasurement {
+	newFrac := em.fraction.Add(&e.fraction)
 	newEm := EnglishMeasurement{Fraction{newFrac.Numerator, newFrac.Denominator}}
-	return &newEm
+	return newEm
 }
 
-func NewEnglishMeasurement(foot, inch int, frac *Fraction) (*EnglishMeasurement) {
-	inches := (foot * 12) + inch;
+func (e *EnglishMeasurement) Subtract(em EnglishMeasurement) EnglishMeasurement {
+	newFrac := em.fraction.Subtract(&e.fraction)
+	newEm := EnglishMeasurement{Fraction{newFrac.Numerator, newFrac.Denominator}}
+	return newEm
+}
+func NewEnglishMeasurement(foot, inch int, frac *Fraction) EnglishMeasurement {
+	inches := (foot * cFOOT) + inch;
 	newFrac := Fraction{
-		Numerator: (inches * frac.Denominator),
+		Numerator: (inches * frac.Denominator + frac.Numerator),
 		Denominator: frac.Denominator,
 	}
 	newEm := EnglishMeasurement{newFrac}
-	return &newEm
+	return newEm
 }
 
 func (e *EnglishMeasurement) GetInch() int  {
-	return e.Fraction.GetInteger()
+	return e.fraction.GetInteger() % cFOOT
 }
 
 func (e *EnglishMeasurement) GetFoot() int {
-	return e.GetInch() / cFOOT
+	return e.fraction.GetInteger() / cFOOT
+}
+
+
+func (e *EnglishMeasurement) GetFractionInch() Fraction {
+	return Fraction{
+		Numerator: e.fraction.Numerator % e.fraction.Denominator ,
+		Denominator: e.fraction.Denominator,
+	}
 }
--- test/EnglishMeasurement_test.go
+++ test/EnglishMeasurement_test.go
@@ -19,11 +19,29 @@ func TestAdd(t *testing.T) {
 
 	em3 := em.Add(em2);
 
+	fmt.Println(em3.GetFractionInch())
 	testEngMes(12, 10, Fraction{3,4}, em3, t)
 
 }
 
-func testEngMes(foot, inch int, frac Fraction, em *EnglishMeasurement, t *testing.T) {
+func TestSubtract(t *testing.T) {
+	em := NewEnglishMeasurement(4, 6, &Fraction{
+		Numerator: 1,
+		Denominator: 2,
+	})
+
+	em2 := NewEnglishMeasurement(8,4, &Fraction{
+		Numerator: 1,
+		Denominator: 4,
+	})
+
+	em3 := em2.Subtract(em);
+
+	fmt.Println(em3.GetFractionInch())
+	testEngMes(3, 9, Fraction{3,4}, em3, t)
+
+}
+func testEngMes(foot, inch int, frac Fraction, em EnglishMeasurement, t *testing.T) {
 	if foot != em.GetFoot() {
 		t.Error(fmt.Sprintf("Expected Foot %d but got ", foot), em.GetFoot());
 	}
@@ -32,11 +50,11 @@ func testEngMes(foot, inch int, frac Fraction, em *EnglishMeasurement, t *testin
 		t.Error(fmt.Sprintf("Expected Inch %d but got ", inch), em.GetInch());
 	}
 
-	if frac.Denominator != em.Fraction.Denominator {
-		t.Error(fmt.Sprintf("Expected Denominator %d but got ", frac.Denominator), em.Fraction.Denominator);
+	if frac.Denominator != em.GetFractionInch().Denominator {
+		t.Error(fmt.Sprintf("Expected Denominator %d but got ", frac.Denominator), em.GetFractionInch().Denominator);
 	}
 
-	if frac.Numerator != em.Fraction.Numerator {
-		t.Error(fmt.Sprintf("Expected Denominator %d but got ", frac.Numerator), em.Fraction.Numerator);
+	if frac.Numerator != em.GetFractionInch().Numerator {
+		t.Error(fmt.Sprintf("Expected Numerator %d but got ", frac.Numerator), em.GetFractionInch().Numerator);
 	}
 }
