0efdd6e68965168830f6a73075019022930964f4
xach <zironside@hotmail.com>
2017-06-08 21:15:06 -0400

    add passing test for parser

--- engmes/Parse.go
+++ engmes/Parse.go
@@ -1,5 +1,11 @@
 package engmes
 
+import (
+	"regexp"
+	"strings"
+	"strconv"
+	"fmt"
+)
 // Parse to parse string
 type Parse struct {
 	Input string
@@ -7,9 +13,61 @@ type Parse struct {
 
 // FromString parse from string
 func ParseString(st string) EnglishMeasurement {
-	return EnglishMeasurement{
-		Fraction{
-			Numerator:   1,
-			Denominator: 2,
-		}}
+
+	foot := parseFoot(st)
+	inch := parseInch(st)
+	fraction := parseFraction(st);
+	return NewEnglishMeasurement(foot, inch, &fraction)
+}
+
+func parseFoot(st string) (int) {
+	footReg := regexp.MustCompile("[0-9]+'");
+
+	if footReg.MatchString(st) {
+		replace := strings.Replace(footReg.FindString(st), "'", "", 1);
+		i, err := strconv.Atoi(replace)
+		if err != nil {
+			fmt.Println(err)
+		}else {
+			return i
+		}
+	}
+
+	return 0
+}
+
+func parseInch(st string) (int) {
+	reg := regexp.MustCompile("[0-9]+\"");
+
+	if reg.MatchString(st) {
+		replace := strings.Replace(reg.FindString(st), "\"", "", 1);
+		i, err := strconv.Atoi(strings.TrimSpace(replace))
+		if err != nil {
+			fmt.Println(err)
+		}else {
+			return i
+		}
+	}
+
+	return 0
+}
+
+func parseFraction(st string) (Fraction) {
+	reg := regexp.MustCompile("[0-9]+/[0-9]+");
+
+	if reg.MatchString(st) {
+		split := strings.Split( reg.FindString(st), "/")
+		i, err := strconv.Atoi(strings.TrimSpace(split[0]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		i2, err2 := strconv.Atoi(strings.TrimSpace(split[1]))
+		if err2 != nil {
+			fmt.Println(err)
+		}
+
+		return Fraction{i,i2}
+	}
+
+	return Fraction{0,1}
 }
--- test/parse_test.go
+++ test/parse_test.go
@@ -12,7 +12,33 @@ func TestParse(t *testing.T) {
 
 	em := ParseString(teststring)
 
-	if em.GetInch() != 4 {
-		t.Error(fmt.Sprintf("Inch should be 4 and its %d ", em.GetInch()))
+	compair(NewEnglishMeasurement(2,4,&Fraction{1,2}), em, t);
+
+}
+
+func TestParseNoSpace(t *testing.T) {
+
+	teststring := "2'4\"1/2"
+
+	em := ParseString(teststring)
+
+	compair(NewEnglishMeasurement(2,4,&Fraction{1,2}), em, t);
+
+}
+func compair(em, testEM EnglishMeasurement, t *testing.T) {
+
+	if em.GetInch() != testEM.GetInch() {
+		t.Error(fmt.Sprintf("Inch should be %d and its %d ", em.GetInch(), testEM.GetInch()))
+	}
+
+	if em.GetFoot() != testEM.GetFoot() {
+		t.Error(fmt.Sprintf("Foot should be %d and its %d", em.GetFoot(), testEM.GetFoot()))
+	}
+
+	if em.GetFractionInch().Numerator != testEM.GetFractionInch().Numerator {
+		t.Error(fmt.Sprintf("Numerator should be %d and its %d", em.GetFractionInch().Numerator, testEM.GetFractionInch().Numerator))
+	}
+	if em.GetFractionInch().Denominator != testEM.GetFractionInch().Denominator {
+		t.Error(fmt.Sprintf("Numerator should be %d and its %d", em.GetFractionInch().Denominator, testEM.GetFractionInch().Denominator))
 	}
 }
